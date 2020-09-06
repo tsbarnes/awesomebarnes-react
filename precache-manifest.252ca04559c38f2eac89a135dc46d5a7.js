d51231ae0a4896b35bc722c86f28fbe57ae604a5
Thea Barnes <thea@tsbarnes.com>
2020-09-06 15:20:27 -0400

    Updates

---
+++ precache-manifest.252ca04559c38f2eac89a135dc46d5a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fd312a634f44cf372fd87afd1906a03",
+    "url": "/awesomebarnes-react/index.html"
+  },
+  {
+    "revision": "425d7125ee4edd354579",
+    "url": "/awesomebarnes-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "25cd00f38e9734912218",
+    "url": "/awesomebarnes-react/static/js/2.df162e31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/awesomebarnes-react/static/js/2.df162e31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "425d7125ee4edd354579",
+    "url": "/awesomebarnes-react/static/js/main.025c297e.chunk.js"
+  },
+  {
+    "revision": "ab89c934af273bb8b3a1",
+    "url": "/awesomebarnes-react/static/js/runtime-main.515c8429.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/awesomebarnes-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
